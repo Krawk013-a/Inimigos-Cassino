0c1f05fdb2e683301a5fb660da74e2a71b116724
Enzo <enzosilva0880@gmail.com>
2025-04-03 20:14:07 -0300

    Revert "Fase de Testes - 11"

--- common.js
+++ common.js
@@ -36,9 +36,6 @@ function registrarTransacao(descricao, valorBruto, isGanho = false) {
     
     localStorage.setItem('extrato', JSON.stringify(extrato));
     
-    // Atualiza a tela
-    updateSaldo();
-    
     return {
         liquido: valorLiquido,
         taxa: taxa
@@ -73,15 +70,13 @@ function finalizarJogoComVitória(ganhoBruto, nomeJogo) {
                      `Valor creditado: R$${resultado.liquido.toFixed(2)}`;
     
     alert(mensagem);
+    updateSaldo();
     
     // Atualiza a mensagem de resultado se existir na página
     const resultadoElement = document.getElementById('resultado');
     if (resultadoElement) {
         resultadoElement.innerHTML = 
-            `🎉 ${nomeJogo}: Ganho de R$${resultado.liquido.toFixed(2)}<br>` +
+            `Você ganhou R$${resultado.liquido.toFixed(2)}<br>` +
             `<small>Taxa de 12.5%: R$${resultado.taxa.toFixed(2)}</small>`;
-        resultadoElement.className = 'ganho';
     }
-    
-    updateSaldo();
 }
