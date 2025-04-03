35a4c15535c7000ba22f93f391cd72bdc83e735a
Enzo <enzosilva0880@gmail.com>
2025-04-03 20:23:13 -0300

    Vamos por favor

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-// supabase-config.js
-
-// Configurações do Supabase (essas variáveis podem vir de um arquivo de ambiente)
-export const supabaseUrl = "https://jdoxsqgkwnlljycibjhd.supabase.co";
-export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impkb3hzcWdrd25sbGp5Y2liamhkIiwicm9zZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MzcxNDY0NSwiZXhwIjoyMDU5MjkwNjQ1fQ.oX7rnMXF3L_m7m9wyHY1re5Y5VLpw4GdV-m63JuiycE";
